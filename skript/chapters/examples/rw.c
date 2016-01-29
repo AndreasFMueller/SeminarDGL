034ce580828ac7c1267bc97faa293e9148f721ef
Andreas Mueller <afm@othello.ch>
2016-01-29 22:15:59 +0100

    updates

--- skript/chapters/examples/rw.c
+++ skript/chapters/examples/rw.c
@@ -49,28 +49,83 @@ int	df(double x, const double y[], double *dfdy, double dfdx[], void *params) {
 	return GSL_SUCCESS;
 }
 
+int	F(double x, const double J[], double F[], void *params) {
+	for (int i = 0; i < 2; i++) {
+		for (int j = 0; j < 4; j++) {
+			F[i * 4 + j] = J[(i + 2) * 4 + j];
+		}
+	}
+	for (int i = 2; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			F[i * 4 + j] = 0;
+		}
+	}
+	return GSL_SUCCESS;
+}
+
 static inline double	sqr(double x) { return x * x; }
 
 int	main(int argc, char *argv[]) {
-	gsl_odeiv2_system	sys = { f, df, 4, &g };
+	gsl_odeiv2_system	system = { f, df, 4, &g };
+	gsl_odeiv2_system	Jsystem = { F, NULL, 16, NULL };
+
 	gsl_odeiv2_driver	*driver
-		= gsl_odeiv2_driver_alloc_y_new(&sys, gsl_odeiv2_step_rkf45,
+		= gsl_odeiv2_driver_alloc_y_new(&system, gsl_odeiv2_step_rk8pd,
 			1e-6, 1e-6, 0.0);
+	gsl_odeiv2_driver	*Jdriver
+		= gsl_odeiv2_driver_alloc_y_new(&Jsystem, gsl_odeiv2_step_rk8pd,
+			1e-6, 1e-6, 0.0);
+	
+
 	double	x = 0.0;
 	double	vx = 8;
 	double	vy = 7;
-	double	y[4] = { 0.0, 0.0, vx, vy };
-	double	deltax = 0.01;
-	for (int i = 1; i <= 200; i++) {
-		double	xnext = i * deltax;
+	double	xnext = 20 / vx;
+	double	delta;
+	int	counter = 0;
+	printf(" n     v_y        x     y[0]     y[1]        dy/dv_y        vynew          delta\n");
+	do {
+		
+		// compute the solution up to x = 1.5
+		x = 0;
+		double	y[4] = { 0.0, 0.0, vx, vy };
 		int	status = gsl_odeiv2_driver_apply(driver, &x, xnext, y);
 		if (status != GSL_SUCCESS) {
 			fprintf(stderr, "error: return value = %d\n", status);
 		}
-		double	yexakt = vx * x - 0.5 * sqr(x) * g;
-		double delta = y[0] - yexakt;
-		printf("%5.2f& %12.8f %12.8f& %12.8f& %12.8f\\\\\n",
-			 x,    y[0], y[1],  yexakt, delta);
+
+		// compute the jacobi matrix up to x = 1.5
+		x = 0;
+		double	J[16] = {	1.0, 0.0, 0.0, 0.0,
+					0.0, 1.0, 0.0, 0.0,
+					0.0, 0.0, 1.0, 0.0,
+					0.0, 0.0, 0.0, 1.0 };
+		status = gsl_odeiv2_driver_apply(Jdriver, &x, xnext, J);
+		if (status != GSL_SUCCESS) {
+			fprintf(stderr, "error: return value = %d\n", status);
+		}
+
+#if 0
+for (int i = 0; i < 4; i++) {
+	for (int j = 0; j < 4; j++) {
+		printf("%12.8f  ", J[i * 4 + j]);
 	}
+	printf("\n");
+}
+printf("\n");
+#endif
+
+		// the derivative is J_{24}
+		double	 derivative = J[1 * 4 + 3];
+
+		// compute the newton correction
+		double	vynew = vy - y[1] / derivative;
+
+		printf("%2d%8.4f %8.4f %8.4f %10.6f %12.8f %12.8f %14.10f\n",
+			 counter, vy, x, y[0], y[1], derivative, vynew, vy-vynew);
+		delta = vy - vynew;
+		vy = vynew;
+		counter++;
+	} while ((fabs(delta) > 1e-6) && (counter <= 50));
 	return EXIT_SUCCESS;
 }
