1ff8180ee18364b92fc299483586a1dac0fedc08
Hansruedi Patzen <hp@revy.ch>
2016-03-31 09:32:32 +0200

    better config

--- skript/wellen/octave/config.m
+++ skript/wellen/octave/config.m
@@ -1,5 +1,7 @@
 akmax = 1000;
-kmax = 225;
+kmax = 300;
+
+xmin = 0;
 xmax = 8;
 scalefactor = 100;
 
@@ -7,10 +9,47 @@ A = 1;
 B = 0;
 C = -1;
 
-parabolaformula = '2;x^2-1  ;';
-yformula = '3;y(x), y(0) = 1, y''(0) = 0;';
-
 a0 = 1; # y(0)
 a1 = 0; # y'(0)
 
-save("./config.dat", "akmax", "kmax", "xmax", "scalefactor", "A", "B", "C", "parabolaformula", "yformula", "a0", "a1")
+parabolaformula = '';
+
+signb = '+';
+signc = '+';
+
+if (B < 0)
+    signb = '';
+endif
+
+if (C < 0)
+    signc = '';
+endif
+
+afactor = '';
+bfactor = '';
+cfactor = '';
+
+if (A != 0)
+    if (A == 1)
+        afactor = 'x^2';
+    else
+        afactor = sprintf('%dx^2', A);
+    endif
+endif
+
+if (B != 0)
+    if (B == 1)
+        bfactor = 'x';
+    else
+        bfactor = sprintf('%s%dx', signb, B);
+    endif
+endif
+
+if (C != 0)
+    cfactor = sprintf('%s%d', signc, C);
+endif
+
+parabolaformula = sprintf('2;%s%s%s   ;', afactor, bfactor, cfactor);
+yformula = sprintf('3;y(x), y(0) = %d, y''(0) = %d;', a0, a1);
+
+save("./config.dat", "akmax", "kmax", "xmin", "xmax", "scalefactor", "A", "B", "C", "parabolaformula", "yformula", "a0", "a1");
--- skript/wellen/octave/yx.m
+++ skript/wellen/octave/yx.m
@@ -1,21 +1,21 @@
 load("./config.dat");
 load("./ak.dat");
 
-datasize = 2 * scalefactor * xmax;
+datasize = scalefactor * (xmax - xmin);
 
-xaxis = linspace(-xmax, xmax, datasize + 1);
+xaxis = linspace(xmin, xmax, datasize + 1);
 y = zeros(datasize, 1);
 p = zeros(datasize, 1);
-for x = (-xmax:1 / scalefactor:xmax)
-    point = a0 + a1 * x;
+for x = (xmin:1 / scalefactor:xmax)
+    yx = a0 + a1 * x;
     for k = (2:kmax)
         akminus2 = a(k + 1);
         akminus3 = a(k);
         akminus4 = a(k - 1);
-        point += -1 / (k * (k - 1)) * (A * akminus4 + B * akminus3 + C * akminus2) * x^k;
+        yx += -1 / (k * (k - 1)) * (A * akminus4 + B * akminus3 + C * akminus2) * x^k;
     endfor
-    p(round((x + xmax) * scalefactor) + 1) = A * x^2 + B * x + C;
-    y(round((x + xmax) * scalefactor) + 1) = point;
+    p(round((x - xmin) * scalefactor) + 1) = A * x^2 + B * x + C;
+    y(round((x - xmin) * scalefactor) + 1) = yx;
 endfor
 
 save yx.dat y
