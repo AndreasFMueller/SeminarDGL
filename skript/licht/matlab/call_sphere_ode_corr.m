b150535b283fdd26151946a68596a2f13f971f33
tiborschneider <tiborschneider@bluewin.ch>
2016-05-16 15:12:20 +0200

    presentation added, changes to text

--- skript/licht/matlab/call_sphere_ode_corr.m
+++ skript/licht/matlab/call_sphere_ode_corr.m
@@ -4,6 +4,7 @@ function [phi, r] = call_sphere_ode_corr( r0, dr0, phimax)
     
     mu = 1;
     sigma = 2;
+    radius = 1001
     initial = [r0, dr0];
     span = [0, phimax];
     [phi, r] = ode45(@ode, span, initial);
@@ -15,24 +16,34 @@ function [phi, r] = call_sphere_ode_corr( r0, dr0, phimax)
     end
 
     function n = fn(r)
-        n = 1 + mu * exp(-sigma*(r-1));
+        %n = 1 + mu * exp(-sigma*(r-100));
         %n = 1;
         %n = 1 + mu * exp(-(r-2)^2*sigma);
-        %n = 1 + mu * (2-r)^2;
+        
+        if r<radius 
+            n = 1 + mu * (radius-r)^2;
+        else
+            n = 1;
+        end
     end
 
     function dn = fdn(r)
-        dn = -sigma * mu * exp(-sigma * (r-1));
+        %dn = -sigma * mu * exp(-sigma * (r-100));
         %dn = 0;
         %dn = -2 * mu * sigma * (r - 2) * exp(-sigma * (r-2)^2);
-        %dn = 2*mu * (r-2);
+        
+        if r<radius
+            dn = 2*mu * (r-radius);
+        else
+            dn = 0;
+        end   
     end
 
     function [] = print_coord(phi, r)
         scopy = '';
         for m = 1:size(phi)
-           fprintf('(%2.5f, %2.5f) ', (r(m)*cos(phi(m)+pi/2)),(r(m)*sin(phi(m)+pi/2))); 
-           scopy = strcat(scopy, sprintf('(%2.5f,%2.5f) ', (r(m)*cos(phi(m)+pi/2)), (r(m)*sin(phi(m)+pi/2))));
+           fprintf('(%4.0f, %4.0f) ', (r(m)*cos(phi(m)+pi/2)),(r(m)*sin(phi(m)+pi/2))); 
+           scopy = strcat(scopy, sprintf('(%4.0f,%4.0f) ', (r(m)*cos(phi(m)+pi/2)), (r(m)*sin(phi(m)+pi/2))));
         end
         clipboard('copy', scopy);
         fprintf('\n0');
