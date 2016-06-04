3f5ec8b586c0eca04563dbcfc5bc91521b66813d
Andri Hartmann <ahartman@hsr.ch>
2016-06-04 11:05:11 +0200

    Repo Images aufgeräumt

--- skript/friedmann/images/c_negativ.m
+++
@@ -1,6 +0,0 @@
-function da = c_negativ(a,t)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-    da = sqrt(1/a - 1);
-end
-
--- skript/friedmann/images/c_zero.m
+++
@@ -1,6 +0,0 @@
-function da = c_zero(a,t)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-    da = sqrt(1/a);
-end
-
