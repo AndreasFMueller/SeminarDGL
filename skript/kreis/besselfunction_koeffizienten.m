b214a7c0db694aa7340dc8794bd023180784c75b
Fennek94 <benjamin.raeber@gmx.ch>
2016-06-07 13:27:16 +0200

    Angepasst auf ganzzahlige und 0 bis 3 Besselfunktion; Zudem wird neu ein PDF als Ausgabe generiert.

--- skript/kreis/besselfunction_koeffizienten.m
+++ skript/kreis/besselfunction_koeffizienten.m
@@ -1,7 +1,7 @@
 
-r=[0:0.01:7];
+r=[0:0.01:10];
 
-n=[0:0.2:1];
+n=[0:1:3];
 
 figure("visible", "off");
 hold all;
@@ -9,13 +9,13 @@ leg='';
 
 for i=1:length(n)
   plot(r,besselj(n(i),r));
-  leg=strcat(leg,'J',num2str(n(i)),'(r);');
+  leg=strcat(leg,'$J_{',num2str(n(i)),'}(r)$;');
 endfor
 grid on;
-axis([r(1) r(length(r)) -0.5 1.25]);
+axis([r(1) r(length(r)) -0.5 1]);
 legend(strsplit(leg,';'));
-xlabel('Abstand r');
+xlabel('Abstand $r$');
 ylabel('Normierte Amplitude');
 plot(r,zeros(1,length(r)),'--k');
-print('-dpng' , 'besselfunction');
+print('-dpdflatexstandalone' , 'besselfunction');
 close
