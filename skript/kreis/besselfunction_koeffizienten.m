663148e5c7f69c7abe0ec845a3c350f99e25ebda
Fennek94 <benjamin.raeber@gmx.ch>
2016-06-17 15:31:43 +0200

    Schriftgroesse angepasst, damit besser lesbar

--- skript/kreis/besselfunction_koeffizienten.m
+++ skript/kreis/besselfunction_koeffizienten.m
@@ -3,7 +3,7 @@ r=[0:0.01:10];
 
 n=[0:1:3];
 
-figure("visible", "off");
+h = figure("visible", "off");
 hold all;
 leg='';
 
@@ -17,5 +17,5 @@ legend(strsplit(leg,';'));
 xlabel('Abstand $r$');
 ylabel('Normierte Amplitude');
 plot(r,zeros(1,length(r)),'--k');
-print('-dpdflatexstandalone' , 'besselfunction');
+print('-dpdflatexstandalone' , 'besselfunction' , '-F:20');
 close
