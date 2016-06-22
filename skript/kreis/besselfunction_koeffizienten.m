304e13715c1c1ed3fd1d93b7f8505d50fae07eb0
Fennek94 <benjamin.raeber@gmx.ch>
2016-06-22 11:13:24 +0200

    r=0:15 angepasst damit Grid quadratisch

--- skript/kreis/besselfunction_koeffizienten.m
+++ skript/kreis/besselfunction_koeffizienten.m
@@ -1,10 +1,9 @@
 
-r=[0:0.01:10];
+r=[0:0.01:15];
 
 n=[0:1:3];
 
 h = figure("visible", "off");
-%h = figure("visible","on");
 hold all;
 leg='';
 
@@ -13,7 +12,7 @@ for i=1:length(n)
   leg=strcat(leg,'$J_{',num2str(n(i)),'}(r)$;');
 endfor
 grid on;
-axis([r(1) r(length(r)) -0.45 1]);
+axis([r(1) r(length(r)) -0.45 1.05]);
 leg(length(leg)) = ''; %Letztes Semikolon entfernen
 legend(strsplit(leg,';'),"location",'northeastoutside');
 xlabel('Abstand $r$');
